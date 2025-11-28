53c177a605db641a6e20cfea3ae134b037c01ebf
Dubon Ya'ar <10558287+DubonYaar@users.noreply.github.com>
2025-11-28 14:26:01 +0200

    Update package dependencies to use master branch

--- Package.swift
+++ Package.swift
@@ -15,9 +15,9 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/kieranb662/Sliders-SwiftUI.git" , from: "1.0.3"),
-        .package(url: "https://github.com/kieranb662/SwiftUI-Shapes.git", from: "1.0.4"),
-        .package(url: "https://github.com/kieranb662/CGExtender.git", from: "1.0.3")
+        .package(url: "https://github.com/kieranb662/Sliders-SwiftUI.git" , branch: "master"),
+        .package(url: "https://github.com/kieranb662/SwiftUI-Shapes.git", branch: "master"),
+        .package(url: "https://github.com/kieranb662/CGExtender.git", branch: "master")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
